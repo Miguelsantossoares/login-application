cfe40375be8593a0491cd51fa0115d6aad9cf3df
Miguel Santos <enium.miguel@gmail.com>
2024-01-15 16:23:49 -0300

    Login Application Complete

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container luid class="grey lighten-5">
+    <router-view/>
+  </v-container>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-app >
+       <v-main>
+          <v-container fluid fill-height>
+             <v-layout align-center justify-center>
+                <v-flex xs12 sm8 md4>
+                   <v-card class="elevation-12">
+                      <v-toolbar dark color="primary">
+                         <v-toolbar-title>Login form</v-toolbar-title>
+                      </v-toolbar>
+                      <v-card-text>
+                      <form ref="form" @submit.prevent="login()">
+                             <v-text-field
+                               v-model="username"
+                               name="username"
+                               label="Username"
+                               type="text"
+                               placeholder="username"
+                               required
+                            ></v-text-field>
+                            
+                             <v-text-field
+                               v-model="password"
+                               name="password"
+                               label="Password"
+                               type="password"
+                               placeholder="password"
+                               required
+                            ></v-text-field>
+                            <v-btn type="submit" class="mt-4" color="primary" value="log in">Login</v-btn>
+                       </form>
+                      </v-card-text>
+                   </v-card>
+                 
+                </v-flex>
+             </v-layout>
+          </v-container>
+       </v-main>
+    </v-app>
+ </template>
+ 
+ <script>
+
+ 
+ export default {
+   name: "Login",
+   data() {
+     return {
+       username: "",
+       password: "",
+     };
+   },
+   
+   methods: {
+      login() {
+   const { username } = this;
+   this.$router.replace({ name: "dashboard", params:{ username: username} });
+      },
+      
+   },
+   
+ };
+ </script>
